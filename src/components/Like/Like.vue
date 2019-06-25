af71aa17951b922da4a9f95aa521e8850a586353
Eric Lu <eric.lu02@sap.com>
2019-06-25 18:52:20 +0800

    Update getADList API

--- src/components/Like/Like.vue
+++ src/components/Like/Like.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <mt-swipe :auto="4000">
-      <mt-swipe-item>1</mt-swipe-item>
-      <mt-swipe-item>2</mt-swipe-item>
-      <mt-swipe-item>3</mt-swipe-item>
+    <mt-swipe :auto="4000" :for="img in imgs">
+      <mt-swipe-item>{{img}}</mt-swipe-item>
     </mt-swipe>
   </div>
 </template>
@@ -21,8 +19,9 @@ export default {
   created () {
     this.$axios.get('/api/getDiscList/fcgi-bin/fcg_yqqhomepagerecommend.fcg?_=1561458800007&g_tk=5381&uin=0&format=json').then(
       res => {
-        debugger
-        this.imgs = res.data.messages
+        var sliders = res.data.data.slider;
+        var imgs = sliders.map(val => val.picUrl);
+        this.imgs = imgs;
       }
     ).catch(
       error => console.log(error)
